3896ac4cb35f9edf9ab10937be2603ba845adaf4
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2025-09-14 23:44:08 +0530

    transitioned from tailwindcss v3 to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: '',
-  integrations: [tailwind(), react()]
+  vite: { plugins: [tailwindcss()] },
 });
